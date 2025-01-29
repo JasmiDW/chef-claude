656c5dcf1e53d1060c537406b1dbad331b25808f
Nadia Rousseau <nadia.rousseau@serli.com>
2025-01-29 17:16:17 +0100

    feat(component) add ingredients

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,39 @@
+import { useState } from "react"
+
 export default function Main(){
+    const [ingredients, setIngredients] = useState(["Poulet", "Citron", "Olives"])
+    
+
+    function handleSubmit(e){
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        //pour des tableaux
+        setIngredients(prev => [...prev, newIngredient])   
+
+        //pour des objets 
+        /*setContact(prevIngredients => {
+            return {
+                ...prevIngredients,
+                isFavorite: !prevIngredients.isFavorite
+            }
+        })*/
+ 
+    }
+    
     return (
         <>
-            <form className='form-ingredient'>
-                <input type="text" placeholder="exemple: basilic" aria-label="Ajouter ingrédient"></input>
+            <form className='form-ingredient' onSubmit={handleSubmit}>
+                <input name="ingredient" id="input" type="text" placeholder="exemple: basilic" aria-label="Ajouter ingrédient"></input>
                 <button>+ Ajouter un ingrédient</button>
             </form>
+            <h2>Liste des ingrédients</h2>
+            <ul>
+                {ingredients.map((ingredient, index) => 
+                    <li key={index}>{ingredient}</li>)}
+            </ul>
         </>
 
     )
 }
+
